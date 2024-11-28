82c0cc9f10ac11ddf442f3bb1bce872fc1843d89
Andres Strello <andresstrello@gmail.com>
2024-11-28 15:30:09 +0100

    Fix documentation and license

--- R/untidy.R
+++ R/untidy.R
@@ -3,6 +3,11 @@
 # Since tucuyricuy is private package, added here
 # with permission from Andrés Christiansen
 
+
+#' @name untidy
+#' @title Remove tibble attributes
+#' @description This function is not released under the AGPL license. All rights are reserved by Andrés Christiansen.
+#' @license All rights reserved by Andrés Christiansen
 .untidy <- function(x){
   out <- x
   out <- lapply(1:ncol(x),function(X){as.vector(out[,X,drop = TRUE])})
